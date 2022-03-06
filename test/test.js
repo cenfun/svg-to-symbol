4f61a954676085b5bc4e067d025209c1d8f0780d
cenfun <cenfun@qq.com>
2022-03-06 14:17:07 +0800

    add digital-numbers

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const path = require('path');
 const EC = require('eight-colors');
 
 const svgToSymbol = require('../lib');
-//console.log(item);
+
 const result = svgToSymbol({
     name: 'my-icons',
     dirs: [path.resolve(__dirname, 'icons')],
@@ -22,6 +22,27 @@ const result = svgToSymbol({
 });
 
 if (result.metadata) {
+    console.log(EC.green('done'));
+} else {
+    console.log(EC.red('ERROR: Failed to generate svg symbol file'));
+}
+
+
+const dn = svgToSymbol({
+    name: 'digital-numbers',
+    dirs: [path.resolve(__dirname, 'digital-numbers')],
+    outputDir: path.resolve(__dirname, 'dist'),
+
+    outputSvg: 'digital-numbers.svg',
+    // inlineSvg: false,
+
+    outputJson: 'digital-numbers.json',
+
+    outputRuntimeLib: 'digital-numbers.js'
+    
+});
+
+if (dn.metadata) {
     console.log(EC.green('done'));
 } else {
     console.log(EC.red('ERROR: Failed to generate svg symbol file'));
