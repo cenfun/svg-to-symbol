f13645ef04416684cb5267f53bcb580e4f4b9d13
cenfun <cenfun@qq.com>
2022-03-12 20:44:10 +0800

    fix svg attrs

--- test/test.js
+++ test/test.js
@@ -3,47 +3,36 @@ const EC = require('eight-colors');
 
 const svgToSymbol = require('../lib');
 
-const result = svgToSymbol({
-    name: 'my-icons',
-    dirs: [path.resolve(__dirname, 'icons')],
-    outputDir: path.resolve(__dirname, 'dist'),
+const list = ['my-icons', 'digital-numbers', 'hero-icons', 'tabler-icons'];
 
-    // outputSvg: 'icons.svg',
-    // inlineSvg: false,
 
-    // outputJson: 'icons.json',
+list.forEach(item => {
 
-    // outputRuntimeLib: 'icons.js',
+    console.log('====================================================');
+    console.log(`generating ${item} ...`);
 
-    metadata: {
-        key: 'value'
+    const result = svgToSymbol({
+        name: item,
+        dirs: [path.resolve(__dirname, `icons/${item}`)],
+        outputDir: path.resolve(__dirname, 'dist'),
+    
+        outputSvg: `${item}.svg`,
+        // inlineSvg: false,
+
+        outputJson: `${item}.json`,
+
+        outputRuntimeLib: `${item}.js`,
+    
+        metadata: {
+            name: item
+        }
+        
+    });
+    
+    if (result.metadata) {
+        console.log(EC.green('done'));
+    } else {
+        console.log(EC.red('ERROR: Failed to generate svg symbol file'));
     }
     
 });
-
-if (result.metadata) {
-    console.log(EC.green('done'));
-} else {
-    console.log(EC.red('ERROR: Failed to generate svg symbol file'));
-}
-
-
-const dn = svgToSymbol({
-    name: 'digital-numbers',
-    dirs: [path.resolve(__dirname, 'digital-numbers')],
-    outputDir: path.resolve(__dirname, 'dist'),
-
-    outputSvg: 'digital-numbers.svg',
-    // inlineSvg: false,
-
-    outputJson: 'digital-numbers.json',
-
-    outputRuntimeLib: 'digital-numbers.js'
-    
-});
-
-if (dn.metadata) {
-    console.log(EC.green('done'));
-} else {
-    console.log(EC.red('ERROR: Failed to generate svg symbol file'));
-}
