0146c7355fc3871e174ffd0a7489dc46718ec2c6
cenfun <cenfun@qq.com>
2022-03-13 12:18:14 +0800

    support namespace

--- test/test.js
+++ test/test.js
@@ -3,10 +3,7 @@ const EC = require('eight-colors');
 
 const svgToSymbol = require('../lib');
 
-const list = ['my-icons', 'digital-numbers', 'hero-icons', 'tabler-icons'];
-
-
-list.forEach(item => {
+['my-icons', 'digital-numbers'].forEach(item => {
 
     console.log('====================================================');
     console.log(`generating ${item} ...`);
@@ -34,3 +31,30 @@ list.forEach(item => {
     }
     
 });
+
+
+console.log('====================================================');
+console.log('generating namespace icons ...');
+
+const dirs = {
+    ho: path.resolve(__dirname, '../node_modules/heroicons/outline'),
+    hs: path.resolve(__dirname, '../node_modules/heroicons/solid'),
+    t: path.resolve(__dirname, '../node_modules/@tabler/icons/icons')
+};
+ 
+const result = svgToSymbol({
+    name: 'namespace',
+    dirs: dirs,
+    outputDir: path.resolve(__dirname, 'dist'),
+    
+    metadata: {
+        name: 'namespace'
+    }
+        
+});
+    
+if (result.metadata) {
+    console.log(EC.green('done'));
+} else {
+    console.log(EC.red('ERROR: Failed to generate svg symbol file'));
+}
